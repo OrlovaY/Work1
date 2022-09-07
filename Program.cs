af4447b92ff367501f6f0171cb37401ad3de8676
yuliya.orlova <orel.y.v@mail.ru>
2022-09-07 20:19:07 +0300

    added task_1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив 
+//из строк, длина которых меньше, либо равна 3 символам.
+
+string[] A;
+System.Console.WriteLine("Введите массив строк через пробел");
+string? s = Console.ReadLine();
+A = s.Split(' ');
+var A1 = new string[A.Length];
+var index = 0;
+            foreach (var value in A)
+            {
+                if (value.Length <= 3)
+                {
+                    A1[index] = value;
+                    index++;
+                    System.Console.Write($"{value} ");
+                }
+                else 
+                {
+                    System.Console.WriteLine("Нет");
+                }
+                
+                
+            }
+           
+
+
