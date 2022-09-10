222ba8367a6dbdef1a088a1c4e5fce3183c30ed9
yuliya.orlova <orel.y.v@mail.ru>
2022-09-10 19:16:10 +0300

    added task finale2

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 //из строк, длина которых меньше, либо равна 3 символам.
 
 string[] A;
+/*
 System.Console.WriteLine("Введите массив строк через пробел");
 string? s=Console.ReadLine();
 A=s.Split(' ');
@@ -17,6 +18,31 @@ var index=0;
                 }
         }
 System.Console.WriteLine();
-          
+*/
 
-
+//string[] array1 = new string[4] {"Hello", "2", "world", ":-)"}; 
+string[] Mass1 = new string[3] { "Russia", "Denmark", "Kazan" };
+//string[] array1 = new string[4] { "1234", "1567", "-2", "computer science" };
+string[] Mass2 = new string[Mass1.Length];
+void NewMass(string[] Mass1, string[] Mass2)
+{
+    int count = 0;
+    for (int i = 0; i < Mass1.Length; i++)
+    {
+        if (Mass1[i].Length <= 3)
+        {
+            Mass2[count] = Mass1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] Mass)
+{
+    for (int i = 0; i < Mass.Length; i++)
+    {
+        Console.Write($"{Mass[i]}");
+    }
+    Console.WriteLine();
+}
+NewMass(Mass1, Mass2);
+PrintArray(Mass2);
